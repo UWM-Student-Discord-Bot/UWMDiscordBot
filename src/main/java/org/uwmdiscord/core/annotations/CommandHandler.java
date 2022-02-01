3479cb8f0b0c73236bbbaee145ed328f63efb644
Aaron Newman <prestonewman@gmail.com>
2022-02-01 16:59:47 -0700

    fixed it Zach

--- src/main/java/org/uwmdiscord/core/annotations/CommandHandler.java
+++
@@ -1,7 +0,0 @@
-package org.uwmdiscord.core.annotations;
-
-import org.uwmdiscord.core.publishers.events.UWMCommandReceiveEvent;
-
-public interface CommandHandler {
-    void handle(UWMCommandReceiveEvent event);
-}
--- src/main/java/org/uwmdiscord/core/publishers/events/UWMJDAEvent.java
+++
@@ -1,7 +0,0 @@
-package org.uwmdiscord.core.publishers.events;
-
-import net.dv8tion.jda.api.events.GenericEvent;
-
-public interface UWMJDAEvent {
-    GenericEvent getRawEvent();
-}
